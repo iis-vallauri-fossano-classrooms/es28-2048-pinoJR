b62a087282c54b3ec067dcb35010fa2f916229ae
pinoJR <e.ambrogio.3477@vallauri.edu>
2025-04-30 11:38:28 +0200

    Prima implementazione js
    Caricata la matrice, generati i 2 in posizione casuale, fatto le funzioni Arrow...

--- index.js
+++ index.js
@@ -1,11 +1,61 @@
 "use strict"
 
+let iR, iR2;
+let iC, iC2;
 const DIM = 4;
 
-window.onload=function(){
-
+window.onload = function () {
+   caricaMatrice();
 }
 
-function generaNumero(min, max){
-   return Math.floor((max - min) * Math.random()) + min;   
+function caricaMatrice() {
+   generaPosCasuale2();
+
+   const wrapper = document.getElementById("wrapper");
+
+   for (let i = 0; i < DIM; i++) {
+      for (let j = 0; j < DIM; j++) {
+         const div = document.createElement("div");
+         div.classList.add("cella");
+         div.id = `div-${i}-${j}`;
+
+         if (i === iR && j === iC) {
+            div.textContent = "2";
+         } else if (i === iR2 && j === iC2) {
+            div.textContent = "2";
+         }
+
+         wrapper.appendChild(div);
+      }
+   }
+   document.addEventListener('keydown', event => {
+      if ( event.key == 'ArrowUp') {
+         spostaSu();
+      }else if(event.key == 'ArrowDown')
+      {
+         spostaGiù();
+      }else if(event.key == 'ArrowLeft')
+      {
+         spostaSinistra();
+      }else if(event.key == 'ArrowRight')
+      {
+         spostaDestra();
+      }
+  })
+}
+function spostaDestra()
+{
+  
+}
+function generaPosCasuale2() {
+   do {
+      iR = generaNumero(0, DIM);
+      iC = generaNumero(0, DIM);
+      iR2 = generaNumero(0, DIM);
+      iC2 = generaNumero(0, DIM);
+   } while (iR === iR2 && iC === iC2); 
+}
+
+function generaNumero(min, max) {
+   return Math.floor(Math.random() * (max - min)) + min;
 }
